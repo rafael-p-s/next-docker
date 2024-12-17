c81c8267a3af745af23f5163cf548e25bf9097ec
rafael-p-s <rafaelsky321@gmail.com>
2024-12-17 17:37:07 -0300

    Teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col justify-center items-center ">
       <h1 className="font-extralight">Hello Docker</h1>
-      <p>Teste do dia 17/12/2024</p>
+      <p>Teste da garagem...</p>
     </div>
   );
 }

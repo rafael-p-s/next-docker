6322d00ae7eca09f217b08c5ad8969cf69852c0a
isabelaCosta95 <isabelaovc95@gmail.com>
2024-12-17 18:20:30 -0300

    test commit --isabela

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ export default function Home() {
     <div className="h-screen flex flex-col justify-center items-center ">
       <h1 className="font-extralight">Hello Docker</h1>
       <p>Teste do dia 17/12/2024</p>
+      <h3>teste isabela</h3>
     </div>
   );
 }
-
-<h3>teste isabela</h3>

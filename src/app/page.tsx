4cc84a9b503240da3ff3ea536f5594f4525f53bd
isabelaCosta95 <isabelaovc95@gmail.com>
2024-12-18 18:18:52 -0300

    testando css --isabela

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
     <div className="h-screen flex flex-col justify-center items-center bg-red-500">
       <h1 className="font-extralight">Hello Docker</h1>
       <p>Teste do dia 17/12/2024</p>
-      <h3>teste isabela</h3>
-      <p>teste modificando o arquivo</p>
+      <h3 className="text-left w-full">teste isabela</h3>
+      <p className="hover:text-white text-right w-full">teste modificando o arquivo</p>
     </div>
   );
 }
